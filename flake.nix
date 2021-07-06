70a850e9213e28abaa7936b524e3a9b845c887de
Congee <congeec@qq.com>
2021-07-06 10:52:15 -0400

    chore: initial flake.nix & flake.lock

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "scala";
+
+  inputs.nixpkgs.url      = "github:nixos/nixpkgs/nixos-unstable";
+  inputs.flake-utils.url  = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+  flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShell = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          jdk
+          sbt
+          scalafmt
+        ];
+      };
+    }
+  );
+}
